e4eb0d4a6e594fed35d0db5f8b7fa99d0f320c92
Merlijn van Deen <valhallasw@gmail.com>
2011-10-20 19:00:49 +0000

    Added option to read existing config file as base

--- prepare.py
+++ prepare.py
@@ -2,13 +2,24 @@ import sys
 import parser
 import codecs
 
-if len(sys.argv) != 3 or sys.argv[1] == "--help":
-    print "%s <export.xml> <config.py>"
+if len(sys.argv) < 3 or len(sys.argv) > 4 or sys.argv[1] == "--help":
+    print "%s <export.xml> <config.py> [base_config]"
     print ""
     print "JIRA conversion, stage 1: preparation"
     print "Writes bare configuration file to allow CSV export"
+    print "Base values for configuration file can be read from [base_config]"
     sys.exit()
 
+if len(sys.argv) == 4:
+    base_config = __import__(sys.argv[3].split('.py')[0])
+    base_project_id = base_config.project_id
+    base_tracker_ids = base_config.tracker_ids
+    base_date_format = base_config.date_format
+    print "Successfully imported %s" % sys.argv[3]
+else:
+    base_project_id = None
+    base_tracker_ids = {}
+    base_date_format = "%d %B %Y %H:%M:%S"
 
 issues = parser.get_issues(sys.argv[1])
 outfile = sys.argv[2]
@@ -42,13 +53,15 @@ print >> f, "# only the project id and bugtracker ids have to be set"
 print >> f, "# you can get them from the sf.net of the bug trackers: "
 print >> f, "# http://sourceforge.net/tracker/?atid=<tracker id>&group_id=<project id>&func=browse"
 print >> f, ""
-print >> f, "project_id = # <project id>"
+print >> f, "project_id = %r # <project id>" % base_project_id
 print >> f, ""
 
-print >> f, "artifact_ids = {}"
+print >> f, "tracker_ids = {}"
 for artifact_key in get_values['artifact_type']:
-    print >> f, "artifact_ids[%r] = # <%s tracker id>" % (artifact_key, artifact_key)
+    print >> f, "tracker_ids[%r] = %r # <%s tracker id>" % (artifact_key, base_tracker_ids.get(artifact_key, None), artifact_key)
 
+print >> f, ""
+print >> f, "date_format = %r" % base_date_format
 print >> f, ""
 print >> f, "# END OF CONFIGURATION"
 print >> f, ""
@@ -56,5 +69,6 @@ print >> f, "max_comments = %r" % max_comments
 print >> f, "max_attachments = %r" % max_attachments
 print >> f, "get_values = %r" % get_values
 print >> f, "counts = %r" % counts
+print >> f, "source = %r" % sys.argv[1]
 
 print "DONE"
